65e0ac5b5f9153b1f9cb8f30eea85cb8180ed459
Masatoshi Koeda <masatoshi.koeda@bizreach.co.jp>
2019-06-27 11:18:34 +0900

    Created accordion

---
+++ src/accordion/index.ts
@@ -0,0 +1,39 @@
+import $ from 'jquery';
+
+class Accordion {
+  private $trigger = this.$wrapper.find('.jsc-accordion-trigger');
+  private $content = this.$wrapper.find('.jsc-accordion-content');
+  private isOpen = false;
+
+  constructor(private $wrapper: JQuery) {
+    this.bindEvents();
+  }
+
+  private bindEvents(): void {
+    this.$trigger.on('click', () => this.toggle());
+  }
+
+  private toggle(): void {
+    if (this.isOpen) {
+      this.close();
+      return;
+    }
+
+    this.open();
+  }
+
+  private open(): void {
+    const height = this.$content[0].scrollHeight;
+    this.$content.height(height);
+  }
+
+  private close(): void {
+    this.$content.height(0);
+  }
+}
+
+$(() => {
+  $('.jsc-accordion').each(function(this) {
+    new Accordion($(this));
+  });
+});
